8080af57d5a8902c1842b9398228bd42bea4a8ce
Jeff Lindsay <progrium@gmail.com>
2011-05-26 17:57:52 -0700

    added test and coverage commands to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,42 @@
 #!/usr/bin/env python
-
+import os
+from setuptools import Command
 from setuptools import setup
+
+def shell(cmdline):
+    args = cmdline.split(' ')
+    os.execlp(args[0], *args)
+
+class GToolsCommand(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+
+class TestCommand(GToolsCommand):
+    description = "run tests with nose"
+    
+    def run(self):
+        shell("nosetests")
+
+class CoverageCommand(GToolsCommand):
+    description = "run test coverage report with nose"
+    
+    def run(self):
+        shell("nosetests --with-coverage --cover-package=gevent_tools")
 
 setup(
     name='gevent_tools',
-    version='0.1',
+    version='0.1.0',
     author='Jeff Lindsay',
     author_email='jeff.lindsay@twilio.com',
     description='gevent related goodies',
     packages=['gevent_tools'],
     scripts=['scripts/serviced'],
-    install_requires=['gevent', 'setproctitle'],
-    data_files=[]
+    install_requires=['gevent', 'setproctitle', 'nose'],
+    data_files=[],
+    cmdclass={
+        'test': TestCommand,
+        'coverage': CoverageCommand,}
 )
