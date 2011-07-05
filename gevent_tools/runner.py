3c2bc403945840ff982638bef2adbe60a6a3fefb
Jeff Lindsay <progrium@gmail.com>
2011-07-05 12:37:19 -0700

    some refactoring to runner, added ignores, slightly updated todo

--- gevent_tools/runner.py
+++ gevent_tools/runner.py
@@ -1,3 +1,4 @@
+import io
 import optparse
 import os.path
 import sys
@@ -11,44 +12,50 @@ def main():
     """Entry point for serviced console script"""
     Runner().do_action()
 
+def runner_options():
+    parser = optparse.OptionParser()
+    parser.add_option("-C", "--config", dest="config", metavar="<file>",
+                    help="Path to Python script to configure and return service to run")
+    #parser.add_option("-X", "--extend", dest="extensions", metavar="<file/python>", action="append",
+    #                help="Python code or script path to extend over the config script")
+    parser.add_option("-l", "--logfile", dest="logfile", metavar="<logfile>", default="serviced.log",
+                    help="Log to a specified file, - for stdout (default: serviced.log)")
+    parser.add_option("-p", "--pidfile", dest="pidfile", metavar="<pidfile>", default="serviced.pid",
+                    help="Save pid in specified file (default: serviced.pid)")
+    #parser.add_option("-c", "--chroot", dest="chroot", metavar="<chroot>",
+    #                help="Chroot to a directory before running (default: don't chroot)")
+    #parser.add_option("-d", "--rundir", dest="rundir", metavar="<directory>",
+    #                help="Change to a directory before running, but after any chroot (default: .)")
+    #parser.add_option("-u", "--user", dest="user", metavar="<user>",
+    #                help="The user to run as. (default: don't change)")
+    #parser.add_option("-g", "--group", dest="group", metavar="<group>",
+    #                help="The group to run as. (default: don't change)")
+    parser.add_option("-N", "--name", dest="name", metavar="<name>",
+                    help="Name of the process using setprocname. (default: don't change)")
+    #parser.add_option("-m", "--umask", dest="umask", metavar="<mask>",
+    #                help="The (octal) file creation mask to apply. (default: 0077 if daemonized)")
+    return parser
+
 class Runner(daemon.runner.DaemonRunner):
     _args = sys.argv[1:]
-    _opener = open
+    _opener = io.open
     
     def __init__(self):
-        parser = optparse.OptionParser()
-        parser.add_option("-n", "--nodaemon", dest="daemonize", default=True, action="store_false",
-                        help="Don't daemonize (stay in foreground)")
-        parser.add_option("-C", "--config", dest="config", metavar="<file>",
-                        help="Path to Python script to configure and return service to run")
-        parser.add_option("-X", "--extend", dest="extensions", metavar="<file/python>", action="append",
-                        help="Python code or script path to extend over the config script")
-        parser.add_option("-l", "--logfile", dest="logfile", metavar="<logfile>", default="serviced.log",
-                        help="Log to a specified file, - for stdout (default: serviced.log)")
-        parser.add_option("-p", "--pidfile", dest="pidfile", metavar="<pidfile>", default="serviced.pid",
-                        help="Save pid in specified file (default: serviced.pid)")
-        parser.add_option("-c", "--chroot", dest="chroot", metavar="<chroot>",
-                        help="Chroot to a directory before running (default: don't chroot)")
-        parser.add_option("-d", "--rundir", dest="rundir", metavar="<directory>",
-                        help="Change to a directory before running, but after any chroot (default: .)")
-        parser.add_option("-u", "--user", dest="user", metavar="<user>",
-                        help="The user to run as. (default: don't change)")
-        parser.add_option("-g", "--group", dest="group", metavar="<group>",
-                        help="The group to run as. (default: don't change)")
-        parser.add_option("-N", "--name", dest="name", metavar="<name>",
-                        help="Name of the process using setprocname. (default: don't change)")
-        parser.add_option("-m", "--umask", dest="umask", metavar="<mask>",
-                        help="The (octal) file creation mask to apply. (default: 0077 if daemonized)")
-        self.parser = parser
+        self.action_funcs = {
+            'start': '_start',
+            'stop': '_stop',
+            'restart': '_restart',
+            'run': 'run', }
         self.service = None
-        
-        self.parse_args()
         self.app = self
+        
+        self.parse_args(*self.load_config(runner_options()))
         self.daemon_context = daemon.DaemonContext()
         self.daemon_context.stdin = self._open(self.stdin_path, 'r')
-        self.daemon_context.stdout = self._open(self.stdout_path, 'w+')
+        self.daemon_context.stdout = self._open(
+            self.stdout_path, 'a+', buffering=1)
         self.daemon_context.stderr = self._open(
-            self.stderr_path, 'w+', buffering=0)
+            self.stderr_path, 'a+', buffering=1)
 
         self.pidfile = None
         if self.pidfile_path is not None:
@@ -56,23 +63,24 @@ class Runner(daemon.runner.DaemonRunner):
                 self.pidfile_path, self.pidfile_timeout)
         self.daemon_context.pidfile = self.pidfile
     
-    def load_config(self):
-        options, args = self.parser.parse_args(self._args)
+    def load_config(self, parser):
+        options, args = parser.parse_args(self._args)
         if options.config:
             config_file = self._open(options.config, 'r')
             d = {'__file__': options.config}
             exec config_file.read() in d, d
-            self.parser.set_defaults(**d)
-        elif len(args) == 0 or args[0] in ['start', 'restart']:
-            self.parser.error("a configuration file is required to start")
+            parser.set_defaults(**d)
+        elif len(args) == 0 or args[0] in ['start', 'restart', 'run']:
+            parser.error("a configuration file is required to start")
+        # Now we parse args again with the config file settings as defaults
+        return parser.parse_args(self._args)
     
-    def parse_args(self):
-        self.load_config()
-        options, args = self.parser.parse_args(self._args)
+    def parse_args(self, options, args):
         self.options = options
-        if not options.daemonize:
-            self.run()
-            sys.exit(0)
+        try:
+            self.action = args[0]
+        except IndexError:
+            self.action = 'run'
         
         self.stdin_path = '/dev/null'
         self.stdout_path = options.logfile
@@ -80,7 +88,7 @@ class Runner(daemon.runner.DaemonRunner):
         
         self.pidfile_path = os.path.abspath(options.pidfile)
         self.pidfile_timeout = 3
-        self.action = args[0]
+        
         if self.action not in self.action_funcs:
             self._usage_exit(args)
     
@@ -88,8 +96,13 @@ class Runner(daemon.runner.DaemonRunner):
         if self.options.name:
             setproctitle.setproctitle(self.options.name)
         self.service = self.options.service()
-        self.service.catch(SystemExit, lambda e,g: self.service.stop())
+        if hasattr(self.service, 'catch'):
+            self.service.catch(SystemExit, lambda e,g: self.service.stop())
         self.service.serve_forever()
+    
+    def do_action(self):
+        func = self._get_action_func()
+        getattr(self, func)()
 
     def _open(self, *args, **kwargs):
         return self.__class__.__dict__['_opener'](*args, **kwargs)
