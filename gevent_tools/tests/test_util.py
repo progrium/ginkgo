bbd9b6701c3e4a5af7bebe890c83f9f8eb03a81f
Sean McQuillan <mcquillan.sean@gmail.com>
2011-08-08 03:49:37 -0700

    updated tests per style at http://docs.pylonsproject.org/community/testing.html

--- gevent_tools/tests/test_util.py
+++ gevent_tools/tests/test_util.py
@@ -1,39 +1,47 @@
 from nose.tools import with_setup, raises
 
-from gevent_tools import util
-from unittest import TestCase
+def _make_class(super):
+    # don't use type(), it's hard to read
+    from gevent_tools.util import defaultproperty
+    class Foo(super):
+        bar = defaultproperty(set)
+        
+    return Foo
 
-class TestUtil(TestCase):
-    def setUp(self):
-        class Foo(object):
-            bar = util.defaultproperty(set)
-            
-            def baz(self):
-                ret = []
-                for item in self.bar:
-                    ret.append(item)
-                return ret
-                
-        self.foo = Foo()
-        self.fuz = Foo()
+def test_defaultproperty():
+    Foo = _make_class(object)
+    foo = Foo()
+    assert isinstance(foo.bar, set), "foo.bar is a set"
 
-    def test_defaultproperty(self):
-        assert self.foo.bar == set(), "foo.bar is a set"
-        assert isinstance(self.foo.bar, set), "foo.bar is a set"
+def test_default_property_mutable():
+    Foo = _make_class(object)
+    foo = Foo()
+    fuz = Foo()
+    s = foo.bar
+    s2 = fuz.bar
 
-    def test_default_property_mutable(self):
-        s = self.foo.bar
-        s2 = self.fuz.bar
+    s.add(3)
+    s2.add(5)
 
-        s.add(3)
-        s2.add(5)
+    assert foo.bar == set([3]), "can mutate reference"
+    assert fuz.bar == set([5]), "but not all references"
 
-        assert self.foo.bar == set([3]), "can mutate reference"
-        assert self.fuz.bar == set([5]), "but not all references"
+def test_default_is_iterable():
+    Foo = _make_class(object)
+    foo = Foo()
+    assert [x for x in foo.bar] == [], "can iterate over self reference"
 
-    def test_default_is_iterable(self):
-        assert self.foo.baz() == [], "can iterate over self reference"
+def test_add_as_first_dereference_works():
+    Foo = _make_class(object)
+    foo = Foo()
+    foo.bar.add(3)
+    assert foo.bar == set([3]), "previous line throws exceptino on fail"
 
-    def test_add_ass_first_dereference_works(self):
-        self.foo.bar.add(3)
-        assert self.foo.bar == set([3]), "previous line throws exceptino on fail"
+def test_default_subclasses_work():
+    Foo = _make_class(object)
+    class Bar(Foo):
+        pass
+    bar = Bar()
+    bar.bar.add(5)
+
+    assert bar.bar == set([5]), "defaultproperties can find parent classes."
