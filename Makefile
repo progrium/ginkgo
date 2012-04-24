0d934bb7aa7986d72d779dbaa000fe98008a121f
Jeff Lindsay <progrium@gmail.com>
2012-04-23 19:12:57 -0700

    put extra setup.py commands into makefile, put development dependencies into requirements.txt, dropped dependency on ProxyTypes

---
+++ Makefile
@@ -0,0 +1,21 @@
+all: test
+
+test:
+	nosetests
+
+dev:
+	pip install -r requirements.txt
+	python setup.py develop
+
+install:
+	python setup.py install
+
+coverage:
+	nosetests --with-coverage --cover-package=ginkgo
+
+build_pages:
+	export branch=$$(git status | grep 'On branch' | cut -f 4 -d ' '); \
+	git checkout gh-pages; \
+	git commit --allow-empty -m 'trigger pages rebuild'; \
+	git push origin gh-pages; \
+	git checkout $$branch
--- setup.py
+++ setup.py
@@ -1,40 +1,6 @@
 #!/usr/bin/env python
 import os
-from setuptools import Command
 from setuptools import setup, find_packages
-
-def command(fn):
-    """decorator for easily making setuptools commands"""
-    def wrapped():
-        class cmdclass(Command):
-            def initialize_options(self): pass
-            def finalize_options(self): pass
-            user_options = []
-            description = fn.__doc__
-            def run(self): fn()
-        return cmdclass
-    return wrapped
-
-@command
-def test():
-    """run tests with nose"""
-    os.execlp("nosetests", "nosetests")
-
-@command
-def build_pages():
-    """rebuild the website"""
-    os.execlp("bash", "bash", "-c", """\
-        branch=$(git status | grep 'On branch' | cut -f 4 -d ' ')
-        git checkout gh-pages && 
-        git commit --allow-empty -m 'trigger pages rebuild' && 
-        git push origin gh-pages && 
-        git checkout $branch""")
-
-@command
-def coverage():
-    """run test coverage report with nose"""
-    os.execlp("nosetests", "nosetests",
-            "--with-coverage", "--cover-package=ginkgo")
 
 from ginkgo import __version__
 
@@ -45,14 +11,10 @@ setup(
     author_email='jeff.lindsay@twilio.com',
     description='Lightweight service framework',
     packages=find_packages(),
-    install_requires=['gevent>=0.13', 'nose', 'ProxyTypes==0.9'],
+    install_requires=['gevent>=0.13'],
     data_files=[],
     entry_points={
         'console_scripts': [
             'ginkgo = ginkgo.runner:run_ginkgo',
             'ginkgoctl = ginkgo.runner:run_ginkgoctl']},
-    cmdclass={
-        'test': test(),
-        'coverage': coverage(),
-        'build_pages': build_pages(),}
 )
