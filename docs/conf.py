f761472002c13ea3a5b47c16fd22d3d8ffe150e6
Jeff Lindsay <progrium@gmail.com>
2012-04-28 17:50:11 -0700

    need to add parent path to find ginkgo

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 # serve to show the default.
 
 import sys, os
+sys.path.insert(0, os.path.abspath('..'))
 from ginkgo import __version__
 
 # If extensions (or modules to document with autodoc) are in another directory,
