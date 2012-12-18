440b75186506bf9a8badba038068dd97293ea4b8
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-12-17 20:19:30 -0800

    Add trove classifiers to setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,16 @@ setup(
     tests_require=['nose'],
     test_suite='nose.collector',
     data_files=[],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
     entry_points={
         'console_scripts': [
             'ginkgo = ginkgo.runner:run_ginkgo',
